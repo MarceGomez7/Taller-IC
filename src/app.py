5a150e917d8643fc53f2164c86d259bc456420e4
MarceGomez7 <marcelo.gomez.me@gmail.com>
2025-06-10 14:57:59 -0300

    Prueba fallo

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from flask import Flask, render_template_string
 
 def mensaje():
-    return "¡Universidad Tecnológica Nacional!"
+    return "¡Universidad Tecnológica!"
 
 app = Flask(__name__)
 
