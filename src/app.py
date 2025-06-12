d968c3566052b018f2d24ab8e092a6052b71cb1a
MarceGomez7 <marcelo.gomez.me@gmail.com>
2025-06-12 12:06:03 -0300

    CRM-8 #DONE Bugs solucionados

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from flask import Flask, render_template_string
 
 def mensaje():
-    return "¡Universidad!"
+    return "¡Universidad Tecnológica Nacional!"
 
 app = Flask(__name__)
 
